66443f7252851898f3b4b512e83c64418f49cf2c
mateuspedroza <mpcmarques@icloud.com>
2017-12-20 15:44:58 +0100

    FDFD

--- listaplr.pl
+++ listaplr.pl
@@ -195,4 +195,99 @@ zerozeros(A,B):-
     % labeling
     labeling([],[A, B]).
     
+% ======= PROBLEMAS DE LOGICA AVANÇADOS ======
 
+% 1 Sequencia magica
+sequenciaMagica(N, List):-
+    % definir dominio
+
+    length(List, N),
+    MaxSize is N-1,
+    domain(List, 0, MaxSize),
+
+    % aplicar restricoes
+    calculateKV(N, Pairs),
+    global_cardinality(List, Pairs),
+
+    % fazer pesquisa
+    labeling([], List).
+
+calculateKV(Max, Final):-
+   calculateKV([], 0, 0, Max, Final).
+
+calculateKV(Temp, Count, Sum, Max, Final):-
+    Sum < Max,
+    append(Temp, [Count-Count], Temp2),
+    Count1 is Count+1,
+    Sum1 is Sum+Count,
+    calculateKV(Temp2, Count1, Sum1, Max, Final).
+calculateKV(A, _, _, _,A).
+
+% 2 - Hexágono mágico
+hexagonoMagico(List):-
+    % definir variaveis e dominio
+    List = [A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S],
+    domain(List, 1, 19),
+
+    % restricoes
+    all_different(List),
+    maplist(sum38, [[A,B,C], [D,E,F,G], [H,I,J,K,L], [M,N,O,P], [Q,R,S],
+            [H,D,A], [M,I,E,B], [Q,N,J,F,C], [R,O,K,G], [S,P,L],
+            [C,G,L], [B,F,K,P], [A,E,J,O,S], [D,I,N,R], [H,M,Q]]),
+    
+
+
+    % calcular
+    labeling([], List).
+
+sum38(List):- sum(List, #=, 38).
+
+% 3 - Luzes
+/*
+luzes(Minumum):-
+    % definir variaveis e dominios.
+    List = [[A,B,C,D], [E,F,G,H], [I,J,K,L], [M,N,O,P]],
+    flatten(List, FlatList),
+    domain(FlatList, 0,1),
+
+    transpose(List, CollumnsList),*/
+
+% 5 - Tumografia
+tumografia(ListaSomaLinhas, ListaSomaColunas, MatrizFlat):-
+    % definir variaveis e dominio
+    length(ListaSomaLinhas, NLinhas),
+    length(ListaSomaColunas, NColunas),
+    criarMatriz(NLinhas, NColunas, MatrizBinaria),
+    flatten(MatrizBinaria, MatrizFlat),
+    domain(MatrizFlat, 0, 1),
+
+    % definir restricoes
+    restricoesTm(MatrizBinaria, ListaSomaLinhas),
+    transpose(MatrizBinaria, MatrizBinariaColunas),
+    restricoesTm(MatrizBinariaColunas, ListaSomaColunas),
+
+    % fazer pesquisa
+    labeling([], MatrizFlat).
+
+    
+restricoesTm([], []).
+restricoesTm([LinhaBinaria | TBinaria], [HSoma | TSoma]):-
+    sum(LinhaBinaria, #=, HSoma),
+    restricoesTm(TBinaria, TSoma).
+
+criarMatriz(N, M, [H|T]):-
+    N > 0,
+    N1 is N-1,
+    criarLinha(M, H),
+    criarMatriz(N1,M, T).
+
+criarMatriz(0,_, []).
+
+criarLinha(N, [_|T]):-
+    N > 0,
+    N1 is N-1,
+    criarLinha(N1, T).
+
+criarLinha(0, []).
+
+% 6 - Amazons
